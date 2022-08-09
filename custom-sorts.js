403a2505c4c2d00c3b3b1ac15cc81a074b609700
collodrollo <jxaziz0001@zonemail.clpccd.edu>
2022-08-09 15:12:37 -0700

    my first commit

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,5 +1,6 @@
 function ageSort(users) {
   // Your code here
+  return users.sort((user1, user2) => user1.age - user2.age);
 }
 
 function oddEvenSort(arr) {
@@ -10,9 +11,40 @@ function validAnagrams(s, t) {
   // Your code here
 }
 
+function countDigits(num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  let count = 0;
+  while (num) {
+    count += 1;
+    num = Math.floor(num / 10);
+  }
+
+  return count;
+}
+
+function compBase(a, b) {
+  countA = countDigits(a);
+  countB = countDigits(b);
+
+  if (countA === countB) {
+    return a - b;
+  }
+
+  if (countA > countB) {
+    return -1;
+  }
+
+  return 1;
+}
+
 function reverseBaseSort(arr) {
   // Your code here
+  return arr.sort(compBase);
 }
+
 
 function frequencySort(arr) {
   // Your code here
